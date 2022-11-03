44c4b56387d6e83ad52f6f3ab1a7bb1e08ee25c7
Muli <arthurkaku@gmail.com>
2022-11-03 20:59:43 +0300

    js amended

--- js/quiz.js
+++ js/quiz.js
@@ -2,10 +2,10 @@ const quizAnswers = document.querySelectorAll(".ouranswers");
 const submitAnswer = document.querySelector("#submit_ans");
 let marks = 0;
 quizAnswers.forEach(function(jibu){
-jibu.addEventListener("change",function(event){
-    marks += parseInt(event.target.value);
-});
+    jibu.addEventListener("change",function(event){
+         marks += parseInt(event.target.value);
+    });
 });
 submitAnswer.addEventListener("click",function(event){
-    alert("your score is");
-})
+    alert("your score is" + marks);
+});
