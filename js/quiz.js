6fbbfc8f0d67d4abb59e9a836d32490e30ca9826
Muli <arthurkaku@gmail.com>
2022-11-03 20:26:35 +0300

    value assigned

--- js/quiz.js
+++ js/quiz.js
@@ -1,1 +1,9 @@
-const quizAnswers = document.querySelectorAll("answers");
+const quizAnswers = document.querySelectorAll("ouranswers");
+console.log(quizAnswers);
+let marks = 0;
+quizAnswers.forEach(function(jibu){
+console.log("jibu");
+jibu.addEventListener("change",function(event){
+    alert(event.target.value);
+});
+});
