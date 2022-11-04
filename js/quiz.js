701d83b79ce57c810a0d5f0fa1014fa0c2049d67
Muli <arthurkaku@gmail.com>
2022-11-04 15:22:30 +0300

    js amended

--- js/quiz.js
+++ js/quiz.js
@@ -1,19 +1,27 @@
-const quizAnswers = document.querySelectorAll(".ouranswers");
-const submitAnswer = document.querySelector("#submit_ans");
-let marks = 0;
-let total = 0;
-quizAnswers.forEach(function (jibu) {
-    if (parseInt(jibu.value) > 0) {
-        total += parseInt(jibu.value);
-    }
-    else {
-        total = total;
-    }
-    jibu.addEventListener("change", function (event) {
-        marks += parseInt(event.target.value);
+$(document).ready(function () { 
+    const quizAnswers = document.querySelectorAll(".ouranswers");
+    const submitAnswer = document.querySelector("#submit_ans");
+    const finalAnswer = document.querySelector('#finalscore');
+    let marks = 0;
+    let total = 0;
+    quizAnswers.forEach(function (jibu) {
+        if (parseInt(jibu.value) > 0) { total += parseInt(jibu.value); }
+        else { total = total; }
+        // jibu.addEventListener("change", function (event) {
+        //     // marks += parseInt(event.target.value);
+
+        //     //alert("Selected");
+        // });
+    });
+    submitAnswer.addEventListener("click", function (event) {
+        quizAnswers.forEach(function (jibu) {
+            if (jibu.checked) {
+                marks += parseInt(jibu.value);
+            }
+        })
+        let score = (marks / total) * 100;
+        alert("your score is " + score + "%");
+        $("#finalscore").text()
     });
 });
-submitAnswer.addEventListener("click", function (event) {
-    let score = (marks / total) * 100;
-    alert("your score is " + score + "%");
-});
+
