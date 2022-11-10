181bfbed0f712d9cc61fe33632d79fcf8f05be90
Muli <arthurkaku@gmail.com>
2022-11-10 11:58:14 +0300

    added more on js

--- js/quiz.js
+++ js/quiz.js
@@ -1,43 +1,18 @@
-$(document).ready(function () { 
-    const quizAnswers = document.querySelectorAll(".ouranswers");
-    const submitAnswer = document.querySelector("#submit_ans");
-    const finalAnswer = document.querySelector("#finalscore");
-    let marks = 0;
-    let total = 0;
-    quizAnswers.forEach(function (jibu) {
-        if (parseInt(jibu.value) > 0) { total += parseInt(jibu.value); }
-        else { total = total; }
-        jibu.addEventListener("change", function (event) {
-         marks += parseInt(event.target.value);
+const submitAnswer = document.querySelector("#submit_ans") 
+const quizAnswers = document.querySelectorAll(".ouranswers");
+let marks = 0;
+let total = 0;
+quizAnswers.forEach(function (jibu) {
+    if (parseInt(jibu.value) > 0) { total += parseInt(jibu.value); }
 
-        alert("Selected");
-        });
+    else { total = total; }
+    jibu.addEventListener("change", function (event) {
+        marks += parseInt(event.target.value)
+
     });
-    submitAnswer.addEventListener("click", function (event) {
-        quizAnswers.forEach(function (jibu) {
-            if (jibu.checked) {
-                marks += parseInt(jibu.value);
-            }
-        })
-        let score = (marks / total) * 100;
-        alert("your score is " + score + "%");
-        let level = "";
-        if (percentage > 80){
-            level += "Excellently passed";
-        }
-        else if(percentage >= 50 && percentage <= 80 ){
-            level += "Fairly Passed";
-        }
-        else{
-            level += "\not passed, Please retake test.";
-            alert.text("You have scored:" + score +"%"+level);
-        }
-    });
-     $("#finalscore").text("You have scored:" + percentage +"%"+level);
 });
-
+//alert(marks)
 submitAnswer.addEventListener("click", function (event) {
-    let score = (marks/total) * 100;
-    alert("your score is " + score + "%");
+    let score =(marks/total)*100;
+    alert("your score is:" + score +"%")
 });
-
